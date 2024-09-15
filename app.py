70a8a46541cb9499c270fae22b87fa999e563a62
Wes Vanegas <wesleyvanegas13@gmail.com>
2024-09-14 20:18:32 -0500

    Add method POST

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db = SQLAlchemy(app)
+
 
 # Irregular verbs examples data
-
 irregular_verbs = {
     "be": {"past": "was/were", "past_participle": "been", "examples":
            ["I am happy.", "He was here yesterday."]},
@@ -13,19 +16,36 @@ irregular_verbs = {
            ["I eat breakfast.", "He has eaten already"]}
 }
 
-@app.route('/api/verb/<string:verb>', methods=['GET'])
-def get_verb(verb):
-    """ Gets information about an irregular verb """
-    verb_info = irregular_verbs.get(verb.lower())
-    if verb_info:
-        return jsonify(verb_info)
-    else:
-        return jsonify({"error":"Verb no found"}), 404
+class Verb(db.Model):
+    #id = db.Column(db.Integer, primary_key=True)
+    verb = db.Column(db.String(20), primary_key=True)
+    past = db.Column(db.String(20), nullable=False)
+    past_participle = db.Column(db.String(20), nullable=False)
+    example_one = db.Column(db.String(120), nullable=False)
+    example_two = db.Column(db.String(120))
+
+
+@app.route('/api/verbs', methods=['POST'])
+def add_verb():
+    data = request.get_json()
+    new_verb = Verb(verb=data['verb'], past=data['past'], past_participle=data['past_participle'],
+                    example_one=data['example_one'], example_two=data['example_two'])
+    db.session.add(new_verb)
+    db.session.commit()
+    return jsonify({'message': 'Verb added succesfully!'}), 201
 
 @app.route('/api/verbs', methods=['GET'])
 def list_verbs():
-    """ List of all available irregular verbs """
-    return jsonify({"verbs":list(irregular_verbs.keys())})
+    verbs = Verb.query.all()
+    output = []
+    for verb in verbs:
+        output.append({'verb': verb.verb, 'past': verb.past, 'past_participle': verb.past_participle,
+                       'example_one': verb.example_one, 'example_two': verb.example_two})
+    return jsonify(output)
+
+
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
