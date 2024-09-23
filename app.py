4297e1849c34100e53754ea6e26d9641b3543a14
Wes Vanegas <wesleyvanegas13@gmail.com>
2024-09-23 14:56:27 -0500

    Delete connection to mongo

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 import random
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 db = SQLAlchemy(app)
 
 
+
 class Verb(db.Model):
-    #id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, primary_key=True)
     verb = db.Column(db.String(20), primary_key=True)
     past = db.Column(db.String(20), nullable=False)
     past_participle = db.Column(db.String(20), nullable=False)
@@ -16,118 +17,155 @@ class Verb(db.Model):
     example_two = db.Column(db.String(120))
 
 
-@app.route('/api/verb/add', methods=['POST'])
+@app.route("/api/verb/add", methods=["POST"])
 def add_verb():
     data = request.get_json()
-    new_verb = Verb(verb=data['verb'], past=data['past'], past_participle=data['past_participle'],
-                    example_one=data['example_one'], example_two=data['example_two'])
+    new_verb = Verb(
+        verb=data["verb"],
+        past=data["past"],
+        past_participle=data["past_participle"],
+        example_one=data["example_one"],
+        example_two=data["example_two"],
+    )
     db.session.add(new_verb)
     db.session.commit()
-    return jsonify({'message': 'Verb added succesfully!'}), 201
+    return jsonify({"message": "Verb added succesfully!"}), 201
 
-@app.route('/api/verbs/add', methods=['POST'])
+
+
+@app.route("/api/verbs/add", methods=["POST"])
 def add_multiple_verbs():
     data = request.get_json()
     if not isinstance(data, list):
-        return jsonify({'message': 'Request body must be a list of verbs'}), 400
+        return jsonify({"message": "Request body must be a list of verbs"}), 400
 
     failed_verbs = []
 
     for item in data:
-        if 'verb' in item and 'past' in item and 'past_participle' in item and 'example_one' in item:
-            new_verb = Verb(verb=item['verb'], past=item['past'], past_participle=item['past_participle'],
-                            example_one=item['example_one'], example_two=item['example_two'])
+        if (
+            "verb" in item
+            and "past" in item
+            and "past_participle" in item
+            and "example_one" in item
+        ):
+            new_verb = Verb(
+                verb=item["verb"],
+                past=item["past"],
+                past_participle=item["past_participle"],
+                example_one=item["example_one"],
+                example_two=item["example_two"],
+            )
             try:
                 db.session.add(new_verb)
                 db.session.commit()
             except Exception as e:
                 db.session.rollback()
-                failed_verbs.append({'verb': item, 'error': str(e)})
+                failed_verbs.append({"verb": item, "error": str(e)})
         else:
-            failed_verbs.append({'verb':item, 'error': 'Missing name or something'})
+            failed_verbs.append({"verb": item, "error": "Missing name or something"})
 
     if failed_verbs:
-        return jsonify({
-            'message': 'Some verbs could not be added',
-            'failed_verbs': failed_verbs
-        }), 400
+        return (
+            jsonify(
+                {
+                    "message": "Some verbs could not be added",
+                    "failed_verbs": failed_verbs,
+                }
+            ),
+            400,
+        )
     else:
-        return jsonify({'message':'All verbs added succesfully'}), 201
+        return jsonify({"message": "All verbs added succesfully"}), 201
 
 
-@app.route('/api/verbs', methods=['GET'])
+@app.route("/api/verbs", methods=["GET"])
 def list_verbs():
     verbs = Verb.query.all()
     output = []
     for verb in verbs:
-        output.append({'verb': verb.verb, 'past': verb.past, 'past_participle': verb.past_participle,
-                       'example_one': verb.example_one, 'example_two': verb.example_two})
+        output.append(
+            {
+                "verb": verb.verb,
+                "past": verb.past,
+                "past_participle": verb.past_participle,
+                "example_one": verb.example_one,
+                "example_two": verb.example_two,
+            }
+        )
     return jsonify(output)
 
 
-@app.route('/api/verb/<string:verb>', methods=['GET'])
+@app.route("/api/verb/<string:verb>", methods=["GET"])
 def get_verb(verb):
     verb_detail = Verb.query.get(verb)
     if verb_detail:
-        return jsonify({'verb': verb_detail.verb, 'past': verb_detail.past, 'past_participle': verb_detail.past_participle, 'example_one': verb_detail.example_one, 'example_two': verb_detail.example_two})
+        return jsonify(
+            {
+                "verb": verb_detail.verb,
+                "past": verb_detail.past,
+                "past_participle": verb_detail.past_participle,
+                "example_one": verb_detail.example_one,
+                "example_two": verb_detail.example_two,
+            }
+        )
     else:
-        return jsonify({'message': 'Verb not found'}), 404
+        return jsonify({"message": "Verb not found"}), 404
 
 
-@app.route('/api/verb/random', methods=['GET'])
+@app.route("/api/verb/random", methods=["GET"])
 def get_random_verb():
     verbs = Verb.query.all()
 
     if not verbs:
-        return jsonify({'error': 'No verbs found'}), 404
+        return jsonify({"error": "No verbs found"}), 404
 
     random_verb = random.choice(verbs)
 
     output = {
-        'verb': random_verb.verb,
-        'past': random_verb.past,
-        'past_participle': random_verb.past_participle,
-        'example_one': random_verb.example_one,
-        'example_two': random_verb.example_two
+        "verb": random_verb.verb,
+        "past": random_verb.past,
+        "past_participle": random_verb.past_participle,
+        "example_one": random_verb.example_one,
+        "example_two": random_verb.example_two,
     }
 
     return jsonify(output)
 
-@app.route('/api/verb/<string:verb>/delete', methods=['DELETE'])
+
+@app.route("/api/verb/<string:verb>/delete", methods=["DELETE"])
 def delete_verb(verb):
     verb_to_delete = Verb.query.filter_by(verb=verb).first()
     if verb_to_delete:
         db.session.delete(verb_to_delete)
         db.session.commit()
-        return jsonify({'message': 'Verb deleted successfully!'}), 200
+        return jsonify({"message": "Verb deleted successfully!"}), 200
     else:
-        return jsonify({'message': 'Verb not found'}), 404
+        return jsonify({"message": "Verb not found"}), 404
 
 
-@app.route('/api/verb/<string:verb>/update', methods=['PUT'])
+@app.route("/api/verb/<string:verb>/update", methods=["PUT"])
 def update_verb(verb):
     verb_to_update = Verb.query.get(verb)
     if verb_to_update:
         data = request.get_json()
         print(data)
-        if 'verb' in data:
-            verb_to_update.verb = data['verb']
-        if 'past' in data:
-            verb_to_update.past = data['past']
-        if 'past_participle' in data:
-            verb_to_update.past_participle = data['past_participle']
-        if 'example_one' in data:
-            verb_to_update.example_one = data['example_one']
-        if 'example_two' in data:
-            verb_to_update.example_two = data['example_two']
+        if "verb" in data:
+            verb_to_update.verb = data["verb"]
+        if "past" in data:
+            verb_to_update.past = data["past"]
+        if "past_participle" in data:
+            verb_to_update.past_participle = data["past_participle"]
+        if "example_one" in data:
+            verb_to_update.example_one = data["example_one"]
+        if "example_two" in data:
+            verb_to_update.example_two = data["example_two"]
         db.session.commit()
-        return jsonify({'message':'Verb updated successfully!'})
+        return jsonify({"message": "Verb updated successfully!"})
     else:
-        return jsonify({'message': 'Verb not found'}), 404
+        return jsonify({"message": "Verb not found"}), 404
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
